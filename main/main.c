df48123aa731bbeec61341345071430d544de70a
taylanmayckon <eusoutaylann@gmail.com>
2025-11-06 17:18:11 -0300

    Refactor(main): Estrutura base para o projeto na main

--- main/main.c
+++ main/main.c
@@ -9,10 +9,51 @@
 #define I2C_MASTER_NUM I2C_NUM_0
 #define I2C_MASTER_FREQ_HZ 100000
 
-static const char *TAG = "i2c_scanner";
+#define AQQUISITION_INTERVAL 200 // Intervalo de captura de dados em ms
 
-void i2c_scanner_task(void *arg)
-{
+// Estruturas para informaçao dos sensores
+typedef struct{
+    float x;
+    float y;
+    float z;
+} axis_t;
+
+// Dados do MPU6050
+typedef struct{
+    axis_t accel;
+    axis_t gyro;
+    float temperature;
+} MPU_data_t;
+
+// Dados do BMP180
+typedef struct{
+    float temperature;
+    float pressure;
+} BMP180_data_t;
+
+// Dados derivados de fusão sensorial 
+typedef struct{
+    float pitch;
+    float roll;
+    float yaw;
+    float altitude;
+} Orientation_data_t;;
+
+// Estrutura completa dos sensores do GY-87
+typedef struct __attribute__((packed)){
+    MPU_data_t mpu6050;
+    axis_t magnetometer; // HMC5883L
+    BMP180_data_t bmp180;
+    Orientation_data_t orientation;
+} SensorData_t;
+
+// Fila para enviar dados entre tasks
+QueueHandle_t xQueueSensorsData;
+
+
+static const char *TAG_SensorsI2C = "vTaskSensorsI2C";
+// Task para leitura dos sensores
+void vTaskSensorsI2C(void *arg){
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C_MASTER_SDA_IO,
@@ -25,37 +66,23 @@ void i2c_scanner_task(void *arg)
     ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_NUM, &conf));
     ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0));
 
-    while (1)
-    {
-        printf("\nEndereços I2C detectados:\n");
-        printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
-        printf("00:         ");
+    ESP_LOGI(TAG_SensorsI2C, "I2C inicializado com sucesso");
 
-        for (uint8_t addr = 3; addr < 0x78; addr++)
-        {
-            if (addr % 16 == 0)
-                printf("\n%.2x:", addr);
+    SensorData_t sensor_data;
 
-            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-            i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, true);
-            i2c_master_stop(cmd);
+    while (1){
+        
 
-            esp_err_t res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, pdMS_TO_TICKS(50));
-            i2c_cmd_link_delete(cmd);
-
-            if (res == ESP_OK)
-                printf(" %.2x", addr);
-            else
-                printf(" --");
-        }
-
-        printf("\n");
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
-void app_main(void)
-{
-    xTaskCreate(i2c_scanner_task, "i2c_scanner_task", 4096, NULL, 5, NULL);
+
+
+void app_main(void){
+    // Filas
+    xQueueSensorsData = xQueueCreate(10, sizeof(SensorData_t));
+
+    // Tasks
+    xTaskCreate(vTaskSensorsI2C, "vTaskSensorsI2C", 4096, NULL, 5, NULL);
 }
